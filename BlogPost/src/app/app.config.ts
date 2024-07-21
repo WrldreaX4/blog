3d8b0ca633e54f527b544ca3ee818ee69db541ae
WrldreaX4 <157551879+WrldreaX4@users.noreply.github.com>
2024-07-21 22:50:48 +0800

    update

--- BlogPost/src/app/app.config.ts
+++ BlogPost/src/app/app.config.ts
@@ -2,14 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { customInterceptor } from './custom.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(withFetch(), withInterceptors([customInterceptor]))
   ]
 };
